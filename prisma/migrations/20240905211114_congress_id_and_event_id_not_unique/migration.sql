56f8e387241bd3aa3ae86433f2d5da4521e77611
daveazzy <davidsonoliveira47@gmail.com>
2024-09-05 18:35:31 -0300

    congress register fixed

---
+++ prisma/migrations/20240905211114_congress_id_and_event_id_not_unique/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "check_ins_congressId_key";
+
+-- DropIndex
+DROP INDEX "check_ins_eventId_key";
+
+-- DropIndex
+DROP INDEX "check_ins_participantId_congressId_eventId_key";
