1ffacdbcde91a70d55ab2750f13ef22ad3d14ee0
daveazzy <davidsonoliveira47@gmail.com>
2024-10-26 03:07:55 -0300

    prisma disconnect undone

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,6 @@
 import { env } from "@/env";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  log: env.NODE_ENV === 'dev' ? ['query'] : [],
-});
-
-process.on('SIGINT', async () => {
-  await prisma.$disconnect();
-  process.exit(0);
-});
-
-process.on('SIGTERM', async () => {
-  await prisma.$disconnect();
-  process.exit(0);
-});
-
-export default prisma;
+export const prisma = new PrismaClient({
+    log: env.NODE_ENV === 'dev' ? ['query'] : [],
+})
